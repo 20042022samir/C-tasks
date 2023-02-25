fadfe83edb4514def23ddbdf67cbb73990e5a4f8
20042022samir <samirfi@code.edu.az>
2023-02-25 04:47:01 -0800

    homework

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+#region task-1
+//do
+//{
+//    try
+//    {
+//        Console.WriteLine("zehmet olmasa topladiginiz bali daxil edin");
+//        int grade = Convert.ToInt32(Console.ReadLine());
+
+//        if (grade >= 91 && grade <= 100)
+//        {
+//            Console.WriteLine("qiymet--A");
+//        }
+//        else if (grade >= 81 && grade <= 90)
+//        {
+//            Console.WriteLine("qiymet--B");
+//        }
+//        else if (grade >= 71 && grade <= 80)
+//        {
+//            Console.WriteLine("qiymet--C");
+//        }
+//        else if (grade >= 61 && grade <= 70)
+//        {
+//            Console.WriteLine("qiymet--D");
+//        }
+//        else if (grade >= 51 && grade <= 61)
+//        {
+//            Console.WriteLine("qiymet--E");
+//        }
+//        else if (grade > 100 || grade < 0)
+//        {
+//            Console.WriteLine("0-100 arasinda olur");
+//        }
+//        else
+//        {
+//            Console.WriteLine("kesilmisen");
+//        }
+//    }
+//    catch (Exception)
+//    {
+//        Console.WriteLine("duzgun eded daxil et");
+//    }
+//}
+//while (true);
+#endregion
+
+
+#region- task2
+//int[] numbers = { 1, -5, -8, -9, 34, 99, 21 };
+
+//int CounterNegative = 0;
+//int CounterPositive = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//	if (numbers[i] > 0)
+//	{
+//		CounterPositive++;
+//	}
+//	else if (numbers[i] < 0)
+//	{
+//		CounterNegative++;
+//	}
+//}
+//Console.WriteLine(CounterPositive);
+//Console.WriteLine(CounterNegative);
+#endregion
+
+
+#region task-3
+//int[] numbers = { 2, 43, 45, 56, 1, 4, 75, 8, 999, 1 };
+//int biggest = numbers[0];
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > biggest)
+//    {
+//        biggest = numbers[i];
+//    }
+//}
+
+//Console.WriteLine(biggest);
+#endregion
